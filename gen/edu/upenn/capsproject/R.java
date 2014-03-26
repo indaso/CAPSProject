71e29e13d8bbff9f2f8c3616cb53e718df163ef9
indaso <indaso5@gmail.com>
2014-03-26 13:14:36 -0400

    changed MainActivity's link to go to SurveyActivity instead of GuidedActivity

--- gen/edu/upenn/capsproject/R.java
+++ gen/edu/upenn/capsproject/R.java
@@ -30,10 +30,12 @@ public final class R {
     public static final class layout {
         public static final int activity_guided=0x7f030000;
         public static final int activity_main=0x7f030001;
+        public static final int activity_survey=0x7f030002;
     }
     public static final class menu {
         public static final int guided=0x7f070000;
         public static final int main=0x7f070001;
+        public static final int survey=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -41,6 +43,7 @@ public final class R {
         public static final int hello_world=0x7f050005;
         public static final int start_button=0x7f050003;
         public static final int title_activity_guided=0x7f050004;
+        public static final int title_activity_survey=0x7f050006;
         public static final int welcome=0x7f050002;
     }
     public static final class style {
--- src/edu/upenn/capsproject/MainActivity.java
+++ src/edu/upenn/capsproject/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Intent i = new Intent(MainActivity.this, GuidedActivity.class);
+				Intent i = new Intent(MainActivity.this, SurveyActivity.class);
 				startActivity(i);
 			}
 		});
