9dbb0e7868a43bdfee0134dc982f13e82a09488d
indaso <indaso5@gmail.com>
2014-03-26 23:50:19 -0400

    added Background image for MainActivity

--- gen/edu/upenn/capsproject/R.java
+++ gen/edu/upenn/capsproject/R.java
@@ -21,7 +21,8 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int galaxybackground=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
         public static final int action_settings=0x7f080001;
@@ -40,11 +41,11 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int caps_talk=0x7f050002;
         public static final int hello_world=0x7f050005;
         public static final int start_button=0x7f050003;
         public static final int title_activity_guided=0x7f050004;
         public static final int title_activity_survey=0x7f050006;
-        public static final int welcome=0x7f050002;
     }
     public static final class style {
         /** 
