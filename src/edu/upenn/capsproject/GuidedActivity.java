cf3d1392df2db5c37b304aa53b88e625f3c14426
TonyXie <txie145@gmail.com>
2014-05-01 18:53:36 -0400

    parsing conversation from csv

--- src/edu/upenn/capsproject/GuidedActivity.java
+++ src/edu/upenn/capsproject/GuidedActivity.java
@@ -1,5 +1,9 @@
 package edu.upenn.capsproject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 
 import android.os.Bundle;
@@ -15,7 +19,7 @@ public class GuidedActivity extends Activity {
 	int currentStage = 1;
 
 	// value for which to end the conversation. Could change in the future. 
-	final int ENDSTAGE = 28;
+	int endstage = 0;
 	
 	// map to keep track of which prompts to use. First string array entry is giver text
 	// and second array entry is receiver text
@@ -68,63 +72,12 @@ public class GuidedActivity extends Activity {
 		giverName = getIntent().getStringExtra(SurveyActivity.PROVIDER_NAME);
 		receiverName = getIntent().getStringExtra(SurveyActivity.RECIPIENT_NAME);
 		// create all the stages of the conversation
-		conversationStage stage1 = new conversationStage("What's on your mind?", "Listen", "Done", 2, "", 0, "", 0, "", 0);
-		prompts.put(1, stage1);
-		conversationStage stage2 = new conversationStage("", "Did you understand what " + receiverName + " said?", "", 0, "", 0, "Yes", 4, "No", 3);
-		prompts.put(2, stage2);
-		conversationStage stage3 = new conversationStage("Listen", "Ask a clarifying question", "", 0, "", 0, "Done", 5, "", 0);
-		prompts.put(3, stage3);
-		conversationStage stage4 = new conversationStage("Listen", "Tell " + receiverName + " what you heard - just say it back", 
-																																				"", 0, "", 0, "Done", 6, "", 0);
-		prompts.put(4, stage4);
-		conversationStage stage5 = new conversationStage("Answer " + giverName + "'s question", "Listen", "", 0, "", 0, "Done", 6, "", 0);
-		prompts.put(5, stage5);
-		conversationStage stage6 = new conversationStage("Did " + giverName + " understand you?", "", "Yes", 8, "No", 7, "", 0, "", 0);
-		prompts.put(6, stage6);
-		conversationStage stage7 = new conversationStage("Explain what " + giverName + " did not.", "Listen", "Done", 2, "", 0, "", 0, "", 0);
-		prompts.put(7, stage7);
-		conversationStage stage8 = new conversationStage("Do you have more to share?", "", "Yes", 9, "No", 10, "", 0, "", 0);
-		prompts.put(8, stage8);
-		conversationStage stage9 = new conversationStage("Great! Share some more.", "Listen", "Done", 2, "", 0, "", 0, "", 0);
-		prompts.put(9, stage9);
-		conversationStage stage10 = new conversationStage("Listen", "Briefly summarize everything " + receiverName + " talked about during this conversation", 
-																												"", 0, "", 0, "Done", 11, "", 0);
-		prompts.put(10, stage10);
-		conversationStage stage11 = new conversationStage("Did " + giverName + " understand you?", "", "Yes", 12, "No", 7, "", 0, "", 0);
-		prompts.put(11, stage11);
-		conversationStage stage12 = new conversationStage("", "Do you have advice, an opinion, or a story to share?", "", 0, "", 0, "Yes", 13, "No", 14);
-		prompts.put(12, stage12);
-		conversationStage stage13 = new conversationStage("Listen", "Ask " + receiverName + " if it's okay for you to share your advice/opinion/story.", "", 0, "", 0, "Done", 15, "", 0);
-		prompts.put(13, stage13);		
-		conversationStage stage14 = new conversationStage("", "Would you like to offer reassurance or let " + receiverName + " know that you care?", "", 0, "", 0, "Yes", 22, "No", 25);
-		prompts.put(14, stage14);
-		conversationStage stage15 = new conversationStage("Answer " + giverName + "'s question", "Listen", "Done", 16, "", 0, "", 0, "", 0);
-		prompts.put(15, stage15);		
-		conversationStage stage16 = new conversationStage("", "What did " + receiverName + " say help?", "", 0, "", 0, "Yes", 17 , "No", 14);
-		prompts.put(16, stage16);
-		conversationStage stage17 = new conversationStage("Listen", "Say 'I realize I don't know everything about your situation, so tell me if what I say makes no sense to you.", "", 0, "", 0, "Done", 18, "", 0);
-		prompts.put(17, stage17);
-		conversationStage stage18 = new conversationStage("Listen", "Share your advice / opinion / story", "", 0, "", 0, "Done", 19, "", 0);
-		prompts.put(18, stage18);
-		conversationStage stage19 = new conversationStage("How helpful is what " + giverName + " said?", "", "Helpful", 20, "Not Helpful", 21, "", 0, "", 0);
-		prompts.put(19, stage19);
-		conversationStage stage20 = new conversationStage("Explain to " + giverName + " why you feel this way", "Listen", "Done", 12, "", 0, "", 0, "", 0);
-		prompts.put(20, stage20);
-		conversationStage stage21 = new conversationStage("Explain to " + giverName + " why you feel this way", "Listen", "Done", 4, "", 0, "", 0, "", 0);
-		prompts.put(21, stage21);
-		conversationStage stage22 = new conversationStage("Listen", "Offer reassurance or let " + receiverName + " know that you care", "", 0, "", 0, "Done", 23, "", 0);
-		prompts.put(22, stage22);
-		conversationStage stage23 = new conversationStage("Listen", "Thank " + receiverName + " for sharing", "", 0, "", 0, "Done", 24, "", 0);
-		prompts.put(23, stage23);
-		conversationStage stage24 = new conversationStage("Thank " + giverName + " for listening to you and offering support", "", "Done", 28, "", 0, "", 0, "", 0);
-		prompts.put(24, stage24);
-		conversationStage stage25 = new conversationStage("", "Would you like to offer to help " + receiverName + " now or in the future?", "", 0, "", 0, "Yes", 26, "No", 23);
-		prompts.put(25, stage25);
-		conversationStage stage26 = new conversationStage("Listen", "Ask 'How can I help, now or in the future'", "", 0, "", 0, "Done", 27, "", 0);
-		prompts.put(26, stage26);
-		conversationStage stage27 = new conversationStage("Answer " + giverName + "'s question", "Listen", "Done", 23, "", 0, "", 0, "", 0);
-		prompts.put(27, stage27);
-
+		try {
+			createConversationFromCsv();
+		} catch (IOException e) {
+			System.out.println("could not read conversations from csv");
+			e.printStackTrace();
+		}
 
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_guided);
@@ -132,7 +85,7 @@ public class GuidedActivity extends Activity {
 		//flip stuff upside down
 		supportReceiverButton1 = (Button)findViewById(R.id.support_receiver_button1);
 		supportReceiverButton1.setRotation(180);
-		supportReceiverButton1.setText(stage1.receiverButtonText1);
+		supportReceiverButton1.setText(prompts.get(1).receiverButtonText1);
 		
 		supportReceiverButton2 = (Button)findViewById(R.id.support_receiver_button2);
 		supportReceiverButton2.setRotation(180);
@@ -144,7 +97,7 @@ public class GuidedActivity extends Activity {
 
 		supportReceiverPrompt = (TextView) findViewById(R.id.support_receiver_prompt);
 		supportReceiverPrompt.setRotation(180);
-		supportReceiverPrompt.setText(stage1.receiverPrompt);
+		supportReceiverPrompt.setText(prompts.get(1).receiverPrompt);
 
 		supportGiverButton1 = (Button)findViewById(R.id.support_giver_button1);
 		// hide button since there is no text
@@ -157,7 +110,7 @@ public class GuidedActivity extends Activity {
 		supportGiverName.setText(giverName);
 
 		supportGiverPrompt = (TextView) findViewById(R.id.support_giver_prompt);
-		supportGiverPrompt.setText(stage1.giverPrompt);
+		supportGiverPrompt.setText(prompts.get(1).giverPrompt);
 		
 		//listener for button clicks on the prompt buttons
 		supportReceiverButton1.setOnClickListener(new View.OnClickListener() {
@@ -219,7 +172,7 @@ public class GuidedActivity extends Activity {
 	
     // method to check if we should switch to end survey 
 	public boolean checkIfEndStage(int myStage){
-		if (myStage == ENDSTAGE){
+		if (myStage == endstage){
 			Intent i = new Intent(GuidedActivity.this, SurveyActivity.class);
 			i.putExtra(GUIDED_FINISHED, true);
 			setResult(RESULT_OK, i);
@@ -227,6 +180,59 @@ public class GuidedActivity extends Activity {
 			return false;
 		}
 		return true;
+	}
+	
+	// create the conversation stages from the csv 
+	public void createConversationFromCsv() throws IOException{
+		InputStream inputStream = getResources().openRawResource(R.raw.script);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+		String line;
+		while ((line = reader.readLine()) != null) {
+			
+			// do not skip empty csv values 
+			String[] values = line.split(",", -1);
+			int stageLevel = Integer.parseInt(values[0]);
+			
+			// the last stage should be the stage at which the conversation ends
+			endstage = stageLevel;
+			
+			// replace [support provider] place holder with actual name
+			String receiverTemp1 = values[1];
+			String receiverTemp2 = receiverTemp1.replace("[support provider]", giverName);
+			String receiverPrompt = receiverTemp2.replace("[support recipient]", receiverName);
+			
+			String receiverButton1Text = values[2];
+			int receiverButton1Dst = 1;
+			if (!(values[3]).trim().equals("")){
+				receiverButton1Dst = Integer.parseInt(values[3]);
+			} 
+			String receiverButton2Text = values[4];
+			int receiverButton2Dst = 1; 
+			if (!(values[5]).trim().equals("")){
+				System.out.println("value[5] is " + values[5]);
+				receiverButton2Dst = Integer.parseInt(values[5]);
+			}
+			String giverTemp1 = values[6];
+			String giverTemp2 = giverTemp1.replace("[support provider]", giverName);
+			String giverPrompt = giverTemp2.replace("[support recipient]", receiverName);
+			
+			String giverButton1Text = values[7];
+			int giverButton1Dst = 1;
+			if (!(values[8]).trim().equals("")){
+				giverButton1Dst = Integer.parseInt(values[8]);
+			}
+			String giverButton2Text = values[9];
+			int giverButton2Dst = 1;
+			if (!(values[10]).trim().equals("")){
+				giverButton2Dst = Integer.parseInt(values[10]);
+			}
+			
+			// construct the conversation stage 
+			conversationStage myConvo = new conversationStage(receiverPrompt, giverPrompt, receiverButton1Text, receiverButton1Dst,
+																receiverButton2Text, receiverButton2Dst, giverButton1Text, 
+																giverButton1Dst, giverButton2Text, giverButton2Dst);
+			prompts.put(stageLevel, myConvo);
+		}
 	}
 	
     // method for switching between the conversation stages 
